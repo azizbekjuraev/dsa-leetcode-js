dec6d5aa12ec4fdb26d5522b3444fcc2c3e77289
azizbekjuraev <ajuraev98@mail.ru>
2026-03-25 11:11:15 +0500

    add get method

---
+++ doubly-linked-list/dll-get.js
@@ -0,0 +1,132 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
+
+  printList() {
+    let temp = this.head;
+    while (temp !== null) {
+      console.log(temp.value);
+      temp = temp.next;
+    }
+  }
+
+  getHead() {
+    if (this.head === null) {
+      console.log("Head: null");
+    } else {
+      console.log("Head: " + this.head.value);
+    }
+  }
+
+  getTail() {
+    if (this.tail === null) {
+      console.log("Tail: null");
+    } else {
+      console.log("Tail: " + this.tail.value);
+    }
+  }
+
+  getLength() {
+    console.log("Length: " + this.length);
+  }
+
+  makeEmpty() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value)
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (this.length === 0) return undefined;
+    let temp = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      temp.prev = null;
+    }
+
+    this.length--;
+    return temp;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  shift() {
+    if (this.length === 0) return undefined;
+    let temp = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    }
+    else {
+      this.head = this.head.next;
+      this.head.prev = null;
+      temp.next = null;
+    }
+    this.length--;
+    return temp;
+  }
+
+  get(index) {
+    if (index < 0 || index > this.length) return;
+    let counter = 0;
+    while (counter !== index) {
+      this.head = this.head.next;
+      counter++;
+    }
+    return this.head;
+  }
+}
+
+function test() {
+  let myDLL = new DoublyLinkedList(1);
+  myDLL.push(2)
+  myDLL.unshift(4);
+  myDLL.push(222)
+  myDLL.shift();
+  myDLL.get(0);
+}
+
+test();
