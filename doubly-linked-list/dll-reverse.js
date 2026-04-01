dac44a385aa75a677004c998edc30a7a8fb82686
azizbekjuraev <ajuraev98@mail.ru>
2026-04-01 11:45:20 +0500

    add reverse method

---
+++ doubly-linked-list/dll-reverse.js
@@ -0,0 +1,210 @@
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
+
+  printList() {
+    let temp = this.head;
+    while (temp !== null) {
+      console.log(temp.value);
+      temp = temp.next;
+    }
+  }
+
+  getHead() {
+    if (this.head === null) {
+      console.log("Head: null");
+    } else {
+      console.log("Head: " + this.head.value);
+    }
+  }
+
+  getTail() {
+    if (this.tail === null) {
+      console.log("Tail: null");
+    } else {
+      console.log("Tail: " + this.tail.value);
+    }
+  }
+
+  getLength() {
+    console.log("Length: " + this.length);
+  }
+
+  makeEmpty() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value)
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (this.length === 0) return undefined;
+    let temp = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      temp.prev = null;
+    }
+
+    this.length--;
+    return temp;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  shift() {
+    if (this.length === 0) return undefined;
+    let temp = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    }
+    else {
+      this.head = this.head.next;
+      this.head.prev = null;
+      temp.next = null;
+    }
+    this.length--;
+    return temp;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    let counter = 0;
+    while (counter !== index) {
+      this.head = this.head.next;
+      counter++;
+    }
+    return this.head;
+  }
+
+  set(index, value) {
+    let temp = this.get(index);
+
+    if (temp) {
+      temp.value = value;
+      return true
+    }
+    return false
+  }
+
+  insert(index, value) {
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+    if (index < 0 || index > this.length) return false;
+
+    const newNode = new Node(value);
+
+    let before = this.get(index - 1);
+    let after = before.next;
+
+    before.next = newNode;
+    newNode.next = after;
+
+    newNode.prev = before;
+    after.prev = newNode;
+
+    this.length++;
+    return true
+  }
+
+  remove(index) {
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    if (index < 0 || index >= this.length) return undefined;
+
+    let temp = this.get(index);
+
+    temp.prev.next = temp.next;
+    temp.next.prev = temp.prev;
+    temp.next = null;
+    temp.prev = null;
+
+    this.length--;
+    return temp;
+  }
+
+  reverse() {
+    let curr = this.head;
+
+    while (curr !== null) {
+      let temp = curr.prev;
+      curr.prev = curr.next;
+      curr.next = temp;
+      curr = curr.prev;
+    }
+
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+  }
+
+}
+
+function test() {
+  let myDLL = new DoublyLinkedList(1);
+  myDLL.push(2)
+  myDLL.unshift(4);
+  myDLL.push(222)
+  myDLL.shift();
+  myDLL.get(0);
+  let result = myDLL.set(1, 2222);
+
+  if (result) {
+    console.log("Successfully updated!");
+  } else {
+    console.log("Error while updating...")
+  }
+
+  myDLL.insert(4, 77);
+
+  const res = myDLL.remove(2);
+  console.log(res)
+
+  myDLL.reverse();
+}
+
+test();
